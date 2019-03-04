d29dccb6869e1bf97cfee54b141572155fac1063
yf-yang <directoryyf@gmail.com>
2019-03-03 19:15:55 -0800

    remove yaml module dep
    Signed-off-by: yf-yang <directoryyf@gmail.com>

--- auto_connect.py
+++ auto_connect.py
@@ -3,15 +3,51 @@
 
 import os
 import sys
-import yaml
 
 from src import main
 
 import logging.config
 
+# logging configurations
+logging_cfg = { 
+    'disable_existing_loggers': True,
+    'formatters': { 
+        'consoleFormatter': { 
+            'class': 'logging.Formatter',
+            'format': '%(levelname)s %(filename)s:%(lineno)d %(message)s'
+        },
+        'fileFormatter': { 
+            'class': 'logging.Formatter',
+            'datefmt': '%Z %x-%X',
+            'format': '%(asctime)s,%(msecs)03d %(levelname)8s '
+                '%(filename)s:%(lineno)d %(message)s'
+        },
+    },
+    'handlers': { 
+        'consoleHandler': { 
+            'class': 'logging.StreamHandler',
+            'formatter': 'consoleFormatter',
+            'level': 'DEBUG',
+            'stream': 'ext://sys.stdout'
+        },
+        'fileHandler': { 
+            'class': 'logging.FileHandler',
+            'filename': '/tmp/auto_connect.log',
+            'formatter': 'fileFormatter',
+            'level': 'INFO',
+            'mode': 'a'
+        }
+    },
+    'loggers': {
+        'src': {
+            'handlers': ['fileHandler'],
+            # 'handlers': ['fileHandler', 'consoleHandler']
+        }
+    },
+    'version': 1
+}
+
 if __name__ == '__main__':
-    # logging configurations
-    logging_cfg = yaml.load(open('logging.conf.yaml'))
     loglevel = os.environ.get('LOGLEVEL', 'INFO')
     logging_cfg['loggers']['src']['level'] = loglevel
 
